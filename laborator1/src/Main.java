2933b6dd30763e06e788a1e48d2dd441cce0e904
VrenceanSara <vrenceansaram@gmail.com>
2025-02-25 10:45:21 +0200

    o clasa ce afiseaza un mesaj in consola cu spatii

--- laborator1/src/Main.java
+++ laborator1/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Dummie dummie = new Dummie();
 
-        dummie.printMessage("Welcome to MPP lecture!");
+        dummie.printMessage("Welcome\n to MPP lecture!");
+
     }
 }
